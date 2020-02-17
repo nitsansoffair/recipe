afff9afce1408f59d108d136a9d8e7afd67cfcdb
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-17 15:27:08 +0200

    add ingredients

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,9 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingrediensChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingrediensChanged.emit(this.ingredients.slice());
+  }
 }
