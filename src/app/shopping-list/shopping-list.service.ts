68a6d2f5d13b7722162d9011eff3b1cbfcfb0e99
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-18 21:10:42 +0200

    observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import { EventEmitter } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import {Subject} from 'rxjs';
 
 export class ShoppingListService {
-  ingrediensChanged = new EventEmitter<Ingredient[]>();
+  ingrediensChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -15,11 +16,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingrediensChanged.emit(this.ingredients.slice());
+    this.ingrediensChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingrediensChanged.emit(this.ingredients.slice());
+    this.ingrediensChanged.next(this.ingredients.slice());
   }
 }
