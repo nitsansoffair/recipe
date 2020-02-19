1172d64c0beb08fe62ce4f31fe0f9f9dbbeb86ad
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-19 19:44:25 +0200

    select

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import {Subject} from 'rxjs';
 
 export class ShoppingListService {
   ingrediensChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
