e457ff00af378ecc542dcbbe08bcd4af24a5bdb2
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-19 20:07:16 +0200

    delete

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -32,4 +32,9 @@ export class ShoppingListService {
     this.ingredients[index] = newIngredient;
     this.ingrediensChanged.next(this.ingredients.slice());
   }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingrediensChanged.next(this.ingredients.slice());
+  }
 }
