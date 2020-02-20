6961bfac08b5741eac5d669b8825d3826a31494c
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-21 00:49:08 +0200

    fetch

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -11,6 +12,12 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http.put('https://ng-course-recipe-book-49975.firebaseio.com/recipes.json', recipes).subscribe((response) => {
       console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-49975.firebaseio.com/recipes.json').subscribe((recipes) => {
+      this.recipeService.setRecipes(recipes);
     });
   }
 }
